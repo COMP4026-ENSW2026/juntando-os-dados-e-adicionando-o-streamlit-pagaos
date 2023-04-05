fc4929c06ca11034f146ef19990a71f33effe945
Dudu Lacerda <dudu.lacerda.dl@gmail.com>
2023-04-04 22:03:16 -0300

    :sparkles: Adicionadas funcionalidades de geração de json

--- algoritmos_extracao/dudu.py
+++ algoritmos_extracao/dudu.py
@@ -22,22 +22,37 @@ class Dudu:
         for link in links:
             aps.append(self.get_infos(link['href']))
 
-        return json.dumps(aps, ensure_ascii=False)
+        return json.dumps(aps, indent=4, ensure_ascii=False)
 
 
     def get_infos(self, link):
         aux = {}
         page = requests.get(link)
         soup = BeautifulSoup(page.content, "html.parser")
-        aux['titulo'] = soup.find("title").text.splitlines()[0]
-        aux["localizacao"] = '';
-        aux["quartos"] = '';
-        aux["banheiros"] = '';
-        aux["tamanho"] = '';
-        aux["aluguel"] = '';
-        aux["condominio"] = '';
-        aux["contato"] = '';
-        aux["link"] = '';
+
+        titulo = soup.find("title").text.splitlines()[0]
+        aluguel = float(
+            soup
+                .find("h2", class_="elementor-heading-title")
+                .text
+                .replace('R$ ', '')
+                .replace('.', '')
+                .replace(',', '.')
+        )
+        contato = soup.find("a", class_="btn-whatsapp-corretor")
+        contato_formatado = ''
+        if contato != None:
+            contato_formatado = contato.text.strip()
+
+        aux["titulo"] = titulo
+        aux["localizacao"] = titulo.split('alugar no ')[-1]
+        aux["quartos"] = ''
+        aux["banheiros"] = ''
+        aux["tamanho"] = ''
+        aux["aluguel"] = aluguel
+        aux["condominio"] = ''
+        aux["contato"] = contato_formatado
+        aux["link"] = link
 
         return aux
 
@@ -46,4 +61,6 @@ newsletter = Dudu(
     "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",
     "https://www.santamerica.com.br/alugar/apartamento-para-alugar/pr/londrina"
 )
-print(newsletter.get_notices())
+
+with open("./sample.json", "w") as outfile:
+    outfile.write(newsletter.get_notices())
