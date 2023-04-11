fe2ee4c7bac3b49ba1d93f949599e3c3ade8a87b
danitorelli <dani.ctorelli@edu.unifil.br>
2023-04-11 19:51:32 -0300

    anuncios imobiliarios - Dani

--- algoritmos_extracao/dani.py
+++ algoritmos_extracao/dani.py
@@ -15,58 +15,44 @@ soup = BeautifulSoup(page.content, "html.parser")
 url = soup.find_all("a", class_="list__link")
 base_url = "https://www.veneza.com.br"
 
+
+  
+def information(links):
+    
+    page = requests.get(links)
+    soup = BeautifulSoup(page.content, "html.parser")
+    teste = soup.find_all("div", class_="container")
+
+        
+    for div in teste:
+        aux = {}
+        titles = div.find("h1", class_="card__type")
+        locations = div.find("p", class_="card__address")
+        rooms = div.find("div", class_="card__element jetgrid jetgrid--justify-left jetgrid--align-center")
+        sizes = div.find("div", class_="jetgrid jetgrid--align-center")
+        rent = div.find("p", class_="ui__text--green")
+        condominiums = div.find("p", class_="ui__text--blue")
+        phones = ("43-3371-0001")
+
+        aux["titulo"] = titles.text.strip()
+        aux["localizacao"] = locations.text.strip().replace("\n", " ")
+        aux["quartos"] = rooms.text.strip().replace("\n", " ")
+        aux["banheiros"] = ''
+        aux["tamanho"] = sizes.text.strip()
+        aux["aluguel"] = rent.text.strip()
+        aux["condominio"] = ''
+        if condominiums != None:
+            aux["condominio"] = condominiums.text.strip()
+        aux["contato"] = phones
+        aux["link"] = links
+    return(aux)
+        
+json_list = []
 for link in url:
     links = str(base_url) + str(link['href'])
-    print("\n")
-    print(links)
-  
-    def information(link):
-        page = requests.get(links)
-        soup = BeautifulSoup(page.content, "html.parser")
-        teste = soup.find_all("div", class_="container")
+    json_list.append(information(links))
+    #print(information(links))
 
-        for div in teste:
-            titles = div.find("h1", class_="card__type")
-            codigo_anuncio = div.find("p", class_="card__reference")
-            locations = div.find("p", class_="card__address")
-            rooms = div.find("div", class_="card__element jetgrid jetgrid--justify-left jetgrid--align-center")
-            sizes = div.find("div", class_="jetgrid jetgrid--align-center")
-            rent = div.find("p", class_="ui__text--green")
-            condominiums = div.find("p", class_="ui__text--blue")
-            descricao = div.find("p", class_="card__text")
-            phones = ("43-3371-0001")
+#print(json_list)
 
-            try:
-                print(titles.text.strip())
-                print(codigo_anuncio.text.strip())
-                print(locations.text.split())
-                print(rooms.text.split())
-                print(sizes.text.strip())
-                print(rent.text.strip())
-                print(condominiums.text.strip())
-                print(descricao.text.strip())
-                print(phones)
-
-            except:
-                print("none")
-
-
-    information(link)
-
-json_list = []
-for i in range(len(titles)):
-    obj = {}
-    obj["titulo"] = titles[i]
-    obj["localizacao"] = locations[i]
-    obj["quartos"] = rooms[i]
-    obj["banheiros"] = bathrooms[i]
-    obj["tamanho"] = sizes[i]
-    obj["aluguel"] = rent[i]
-    obj["condominio"] = condominiums[i]
-    obj["contato"] = phones[i]
-    obj["link"] = links[i]
-    json_list.append(obj)
-
-filename = "sample.json"
-
-mount_json(filename, json_list)
+mount_json("imoveis.json", json_list)
